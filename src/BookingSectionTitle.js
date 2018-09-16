f932329c4a086f122a34eb631f8efc1dac94f2d3
Martin <cerny.mrtn@gmail.com>
2018-09-16 21:25:57 +0200

    Reproducing bug

--- src/BookingSectionTitle.js
+++ src/BookingSectionTitle.js
@@ -4,7 +4,7 @@ import { Trans } from '@lingui/macro';
 const BookingSectionTitle = () => (
   <div className="is-size-4 has-text-white has-text-weight-semibold">
     <Trans>Book an appointment online</Trans>
-    </div>
+  </div>
 );
 
 export default BookingSectionTitle;
